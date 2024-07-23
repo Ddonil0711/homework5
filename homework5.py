ad6f84b66567c17eab471e69bae37102c07fc224
Ddonil0711 <danil.kremnev.00@mail.ru>
2024-07-23 23:23:35 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,6 @@
+immutable_var = ('watermelon', 1, True, False , 8.2)
+print(immutable_var)
+#immutable_var[0] = 2  "Кортеж относится к неизменяемым типам данных и в данном случае не содержит в себе список"
+mutable_list = [1, 2, 4,'juse', True]
+mutable_list[2] = 'cucumber'
+print(mutable_list)
